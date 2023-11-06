49b61bbfacd0b1f69932e2ce1892d94965aa647f
jamesbraid <james_braid@hotmail.co.uk>
2023-11-06 19:54:21 +0000

    Elements and most variables added. High score and enemy variables not yet added. Event listeners added for arrow keys and start button. Character movement around the grid functions complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,25 @@
 
 //* Elements
-
+const startBtn = document.querySelector('button')
 const grid = document.querySelector('.game-grid')
 const cells = []
-
+const livesDisplay = document.querySelector('.lives-display')
+const scoreDisplay = document.querySelector('.score-display')
+const highScoreDisplay = document.querySelector('.high-score-display')
 
 
 //* Variables
+let lives = 3
+let score = 0
+let gameActive = false
+const startPos = 50
+let currentPos = startPos
 const width = 10
 const cellCount = width * width
 
 
-
 //* Executions
+// Grid creator function
 function createGrid() {
   for (let i = 0; i < cellCount; i++) {
     const cell = document.createElement('div')
@@ -23,20 +30,45 @@ function createGrid() {
     grid.append(cell)
     cells.push(cell)
   }
-  // addCat(currentPos)
+  addChar(currentPos)
 }
 
+// Character movement
+function addChar() {
+  cells[currentPos].classList.add('char')
+}
 
-// * Page Load
+function removeChar() {
+  cells[currentPos].classList.remove('char')
+}
+
+// On keypress update cat position
+function keyPress(evt) {
+  const key = evt.code
+  removeChar()
+  if (key === 'ArrowUp' && currentPos >= width) {
+    currentPos -= width
+  } else if (key === 'ArrowDown' && currentPos + width < cells.length) {
+    currentPos += width
+  } else if (key === 'ArrowLeft' && currentPos % width !== 0) {
+    currentPos--
+  } else if (key === 'ArrowRight' && currentPos % width !== width - 1) {
+    currentPos++
+  }
+  addChar()
+}
+
+// * On Page Load
 createGrid()
 
+// Enemy appearance
 
 
-
-
-
-
+// Enemy movement
 
 
 
 //* Events
+startBtn.addEventListener('click', )
+
+document.addEventListener('keydown', keyPress)
