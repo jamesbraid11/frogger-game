0c17a4a31da87ab2327d67c7b9fe358573283f1a
jamesbraid <james_braid@hotmail.co.uk>
2023-11-06 23:15:25 +0000

    First column that enemies will appear in saved as an array of cells in JS. A pattern of enemies inserted by adding a class to some indexes of the array. Logic created to move the enemies down the array by one grid cell.

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,10 @@
 //* Elements
 const startBtn = document.querySelector('button')
 const grid = document.querySelector('.game-grid')
-const cells = []
+let cells = []
 const livesDisplay = document.querySelector('.lives-display')
 const scoreDisplay = document.querySelector('.score-display')
 const highScoreDisplay = document.querySelector('.high-score-display')
-
 
 //* Variables
 let lives = 3
@@ -42,10 +41,12 @@ function removeChar() {
   cells[currentPos].classList.remove('char')
 }
 
-// On keypress update cat position
+// On keypress update character position
 function keyPress(evt) {
   const key = evt.code
+
   removeChar()
+  
   if (key === 'ArrowUp' && currentPos >= width) {
     currentPos -= width
   } else if (key === 'ArrowDown' && currentPos + width < cells.length) {
@@ -55,20 +56,54 @@ function keyPress(evt) {
   } else if (key === 'ArrowRight' && currentPos % width !== width - 1) {
     currentPos++
   }
+
   addChar()
+
 }
 
 // * On Page Load
 createGrid()
 
 // Enemy appearance
+// Define column in grid as an array
+const enemyCol1 = cells.filter(cell => parseInt(cell.id) % 10 === 1)
+console.log(enemyCol1)
+// Add enemy class to pattern of cells using their index in the array
+enemyCol1[1].classList.add('dark-figure')
+enemyCol1[2].classList.add('dark-figure')
+enemyCol1[4].classList.add('dark-figure')
+enemyCol1[6].classList.add('dark-figure')
+enemyCol1[7].classList.add('dark-figure')
+enemyCol1[9].classList.add('dark-figure')
 
+
+
+// enemyCol1.forEach((cell) => {
+//   if (parseInt(cell.id) === 11, 21) {
+//     cell.classList.add('dark-figure')
+//     console.log(enemyCol1)
+//   }
+// })
+
+// Once enemy classes are added to a pattern of cells, use a forEach to iterate over them and move any instance of that class down the grid by one (using same logic as character movement) Put this in a setInterval so that it happens every n seconds
+
+
+enemyCol1.forEach(function(cell, index) {
+  if (cell.classList.contains('dark-figure')) {
+    cell.classList.remove('dark-figure')
+    cell[index + 1].classList.add('dark-figure')
+  }
+})
+
+// Set enemy class to move along the array
+// Set this to happen at an interval
+// Loop the array back on itself so the sequence and movement continue
 
 // Enemy movement
 
 
 
 //* Events
-startBtn.addEventListener('click', )
+// startBtn.addEventListener('click', )
 
 document.addEventListener('keydown', keyPress)
